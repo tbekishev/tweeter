51709efb7313435392a1a01b9e6c9f6c5ae9f33f
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-09 15:31:38 -0400

    add timeago, add ajax GET to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 function createTweetElement(data) {
-  const newTimestamp = new Date(Date.now() - data.created_at);
   let $tweet = `
   <form class = "tweet"> 
   <header>
@@ -38,7 +14,7 @@ function createTweetElement(data) {
   <p class="text">${data.content.text}</p>
   </article>
   <footer>
-  <p>${(newTimestamp.getDate())} days ago</p>
+  <p>${timeago.format(data.created_at)}</p>
   <div class="icons">
   <i class="fa-solid fa-flag"></i>
   <i class="fa-solid fa-retweet"></i>
@@ -57,7 +33,18 @@ const renderTweets = function(tweets) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+  function loadtweets() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets/",
+      data: "data",
+      success: function (response) {
+        renderTweets(response);
+      }
+    });
+  }
+  loadtweets();
+  //renderTweets(data);
   $("#newTweet").on("submit", function(event) {
     $.ajax({
       type: "POST",
